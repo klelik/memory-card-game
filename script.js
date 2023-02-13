1c7cc0e42aa2dbe353031760bcb8892cb1682005
klelik <klelika1998@gmail.com>
2023-02-13 14:05:15 +0000

    Added Score and fixed bugs

--- script.js
+++ script.js
@@ -1,44 +1,87 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
+const scoreDisplay = document.querySelector(".score");
+const turnsDisplay = document.querySelector(".turns");
+const totalScoreDisplay = document.querySelector(".totalScore");
+
+shuffle();  //initiallize board
+
 
 let hasFlippedCard = false;
+let lockBoard = false; //lock the board so we can not reveal 3rd card if not a match
 let firstCard, secondCard;
+let score = 0;  //initiallize score and display score = 0;
+scoreDisplay.innerHTML = score;
+let turns = 0; //how many times player clicks
+turnsDisplay.innerHTML = turns;
+let totalScore = 0;
+function flipCard() {
+  if (!lockBoard) {
+    //if the board isnt locked -> unflip
 
+    this.classList.add("flip"); //adds class flip when clicked
 
-function flipCard(){
-    this.classList.add('flip');  //ads class flip when clicked
 
     if (!hasFlippedCard) {
-        //first time clicking a card 
-        hasFlippedCard = true;
-        firstCard = this;
+      //first time clicking a card
+      hasFlippedCard = true;
+      firstCard = this;
 
-        // console.log({hasFlippedCard, firstCard});
-    }else{
-        //the second click
-        hasFlippedCard = false;
-        // secondCard = this.childNodes[1].alt; this is how we get the alt of each backphoto
-        secondCard = this;
-        // console.log({firstCard, secondCard});
-        
-        //check if cards match
-        if(firstCard.childNodes[1].alt === secondCard.childNodes[1].alt){
-           
-            //remove clicking again if they match
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-           
-            console.log("MATCH !!!!!!");
-        }else{
-            //not a match
-            
-            setTimeout(() => {  //timeout to see the back 
-                //if not a match flip the cards back
-                firstCard.classList.remove('flip'); 
-                secondCard.classList.remove('flip');
-            },300);
-        }
+    } else {
+      //the second click
+      hasFlippedCard = false;
+      // secondCard = this.childNodes[1].alt; this is how we get the alt of each backphoto
+      secondCard = this;
 
-
+      checkMatch();
     }
+  }
 }
-cards.forEach(card => card.addEventListener('click', flipCard));
+
+function checkMatch() {
+  //check if cards match
+  turns ++;
+  turnsDisplay.innerHTML = turns;
+  if (firstCard.childNodes[1].alt === secondCard.childNodes[1].alt) {
+    disableCards();
+  } else {
+    //not a match
+    unflipCards();
+  }
+}
+
+function disableCards() {
+  //remove clicking again if they match
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  score++;
+  scoreDisplay.innerHTML = score;
+  console.log("MATCH !!!!!!");
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    //timeout to see the back
+    //if not a match flip the cards back
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 400);
+}
+
+function shuffle() { //they are flex items, using order property we give a random number to each card and then they get ordered again in a different way 
+    cards.forEach(card => {
+        let randomNum = Math.floor(Math.random()*12);
+        card.style.order = randomNum;
+    })
+}
+
+function scoreCount() {
+totalScore = (100 * score)/turns
+totalScoreDisplay.innerHTML = totalScore;
+alert("CONGRATULATION Your Score is ")
+
+}
+
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
